89082f1890b3dcf5700a12384fce6b7352740bfd
Greg Granito <gregory.granito@gmail.com>
2021-02-21 19:40:56 -0800

    initial gatsby

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: "96px",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// markup
+const IndexPage = () => {
+  
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <div>Hello Magnet</div>
+    </main>
+  )
+}
+
+export default IndexPage
