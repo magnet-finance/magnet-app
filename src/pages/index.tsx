4ca91c3e23e501044ad17b23f44653948a8c8a65
Will Hennessy <hennessywill@gmail.com>
2021-02-23 02:15:56 -0800

    checkpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,30 @@
 import * as React from "react";
+import "antd/dist/antd.css";
+import { Image, Layout, Menu } from 'antd';
+import logoPng from '../images/icon.png';
+
+const { Header, Content } = Layout;
+
+// markup
+const IndexPage = () => {
+  
+  return (
+    <Layout className="layout">
+      <Header>
+        <Image height={48} width={48} src={logoPng} preview={false} />
+        <Menu mode="horizontal">
+          <Menu.Item key="Mint">Mint</Menu.Item>
+          <Menu.Item key="Review">Review</Menu.Item>
+        </Menu>
+      </Header>
+      <Content  style={{ padding: '64px 146px'}}>
+        <div>Hello world</div>
+      </Content>
+    </Layout>
+  )
+}
+
+export default IndexPage
 
 // styles
 const pageStyles = {
@@ -6,16 +32,3 @@ const pageStyles = {
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-
-// markup
-const IndexPage = () => {
-  
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <div>🟦🟥 Magnet</div>
-    </main>
-  )
-}
-
-export default IndexPage
