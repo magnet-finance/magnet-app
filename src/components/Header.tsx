a224e3674d27ecf33eea33185826498afe49649b
Will Hennessy <hennessywill@gmail.com>
2021-02-24 01:39:48 -0600

    Add app header (#5)
    * checkpoint
    
    * Add header component, design index.tsx, init editorconfig

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Layout } from 'antd';
+import "antd/dist/antd.css";
+import { navigate } from "gatsby";
+import * as React from "react";
+import logoPng from '../images/icon.png';
+
+// markup
+export const Header : React.FC = () => {
+  const goToMint = () => {
+    navigate("/mint");
+  }
+
+  const goToReview = () => {
+    navigate("/review");
+  }
+
+  return (
+    <Layout.Header style={styles.header}>
+      <img src={logoPng} style={styles.logo}/>
+      <div style={styles.spacer}/>
+      <Button
+        onClick={goToMint}
+        style={styles.button}
+        type="primary"
+        size="large"
+        shape="round"
+        icon={<PlusOutlined />}>
+          Mint
+      </Button>
+      <Button
+        onClick={goToReview}
+        style={styles.button}
+        type="text"
+        size="large">
+          Review
+      </Button>
+    </Layout.Header>
+  )
+}
+
+const styles : {[key: string]: React.CSSProperties} = {
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    flex: 0,
+  },
+  spacer: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 24,
+    marginRight: 24,
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
+import { Layout } from 'antd';
+import "antd/dist/antd.css";
 import * as React from "react";
+import { Header } from '../components/Header';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+const { Content } = Layout;
 
 // markup
 const IndexPage = () => {
-  
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <div>🟦🟥 Magnet</div>
-    </main>
+    <Layout>
+      <Header />
+      <Content  style={styles.content}>
+        <div>Hello world</div>
+      </Content>
+    </Layout>
   )
 }
 
 export default IndexPage
+
+const styles : {[key: string]: React.CSSProperties} = {
+  content: {
+    paddingTop: 64,
+    paddingBottom: 64,
+    paddingLeft: 146,
+    paddingRight: 146,
+  }
+}
