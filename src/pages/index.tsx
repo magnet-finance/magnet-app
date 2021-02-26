7b897a7597532aa652acd0209f65856a07d35026
Will Hennessy <hennessywill@gmail.com>
2021-02-26 01:30:20 -0800

    Checkpoint with fonts and container layout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
+import { navigate } from "gatsby";
 import * as React from "react";
 import { Header } from '../components/Header';
 import { ThemeProvider } from '../components/ThemeProvider';
@@ -7,6 +8,10 @@ const { Content } = Layout;
 
 // markup
 const IndexPage = () => {
+  const goToMint = () => {
+    navigate("/mint");
+  }
+
   return (
     <ThemeProvider>
       <Layout>
@@ -14,8 +19,24 @@ const IndexPage = () => {
         <Content  style={styles.content}>
           <div style={styles.title}>Magnetize your Mission.</div>
           <div style={styles.subtitle}>Attract and retain contributors</div>
-          <div style={styles.choose}>Choose a magnet</div>
-          <div></div>
+          <div style={styles.subsubtitle}>Choose a magnet</div>
+          <div style={styles.cardContainer}>
+            <div style={styles.card}>
+              <div style={styles.cardTitle}>
+                Vest
+              </div>
+            </div>
+            <div style={styles.card}></div>
+            <div style={styles.card}></div>
+          </div>
+          <Button
+            onClick={goToMint}
+            style={styles.mintMultipleButton}
+            size="large"
+            type="primary"
+            ghost={true}>
+              Mint Multiple
+          </Button>
         </Content>
       </Layout>
     </ThemeProvider>
@@ -29,24 +50,54 @@ const styles : {[key: string]: React.CSSProperties} = {
     backgroundColor: "#FFFFFF",
     paddingTop: 64,
     paddingBottom: 64,
-    paddingLeft: 146,
-    paddingRight: 146,
+    paddingLeft: 50,
+    paddingRight: 50,
+    textAlign: "center",
   },
   title: {
     fontSize: 56,
+    lineHeight: "64px",
     fontWeight: 600,
-    textAlign: "center",
   },
   subtitle: {
     fontSize: 20,
-    textAlign: "center",
-    color: "#828282",
+    lineHeight: "28px",
+    fontWeight: 300,
+    marginTop: 8,
+    color: "#8C8C8C",
   },
-  choose: {
+  subsubtitle: {
+    fontSize: 24,
+    lineHeight: "32px",
+    fontWeight: 600,
+    color: "#1890FF",
+    marginTop: 54,
+  },
+  cardContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 32,
+  },
+  card: {
+    width: 326,
+    height: 374,
+    backgroundColor: "#F5F5F5",
+    borderRadius: 8,
+    marginLeft: 56,
+    marginRight: 56,
+    textAlign: "center",
+    paddingTop: 24,
+    paddingBottom: 24,
+    paddingLeft: 32,
+    paddingRight: 32,
+
+  },
+  cardTitle: {
     fontSize: 24,
     fontWeight: 600,
-    textAlign: "center",
-    color: "#1890FF",
-    marginTop: 45,
+    marginTop: 24,
+  },
+  mintMultipleButton: {
+    marginTop: 48,
   }
 }
