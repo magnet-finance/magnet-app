b55635a3495cf8cbbd1b019253098ac1fb71b6f0
Greg Granito <gregory.granito@gmail.com>
2021-02-26 00:14:24 -0800

    switched header to new image (#9)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { Button, Layout } from 'antd';
-import { navigate } from "gatsby";
+import { graphql, navigate, useStaticQuery } from "gatsby";
+import Img from 'gatsby-image';
 import * as React from "react";
-import logoPng from '../images/icon.png';
 
 // markup
 export const Header : React.FC = () => {
@@ -18,9 +18,23 @@ export const Header : React.FC = () => {
     navigate("/review");
   }
 
+  const imgData = useStaticQuery(graphql`
+  query {
+    image: file(relativePath: { eq: "icon.png" }) {
+      childImageSharp {
+        fixed(width: 40, height: 40) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`);
+
   return (
     <Layout.Header style={styles.header}>
-      <img src={logoPng} style={styles.logo} onClick={goToIndex}/>
+      <div style={styles.logo} onClick={goToIndex}>
+        <Img fixed={imgData.image.childImageSharp.fixed}/>
+      </div>
       <div>
         <Button
           onClick={goToReview}
