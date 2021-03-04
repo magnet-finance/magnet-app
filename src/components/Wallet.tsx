7cf336245715af077d089e968ac8a0cb2bbae258
Will Hennessy <hennessywill@gmail.com>
2021-03-04 03:24:03 -0600

    Connect a wallet, add web3react and ethers (#19)
    * Connect a wallet, add web3react and ethers
    
    * yarn deduplicate

---
+++ src/components/Wallet.tsx
@@ -0,0 +1,70 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { Button } from "antd";
+import * as React from "react";
+
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds: [
+    1, // Mainet
+    3, // Ropsten
+    4, // Rinkeby
+    5, // Goerli
+    42, // Kovan
+  ],
+})
+
+export const Wallet: React.FC = () => {
+  const { account, activate, active, deactivate } = useWeb3React<Web3Provider>()
+
+  const handleClickWhileActive = () => {
+    deactivate();
+  }
+
+  const handleClickWhileInactive = () => {
+    activate(injectedConnector);
+  }
+
+  const getShortAddress = () => {
+    if (account != null) {
+      if (account.length > 12) {
+        return `${account.substring(0, 6)}...${account.substring(account.length - 4)}`;
+      } else {
+        return account;
+      }
+    } else {
+      return "-";
+    }
+  }
+
+  return (
+    <>
+      {active ? (
+        <Button
+          onClick={handleClickWhileActive}
+          style={styles.walletButton}
+          size="large">
+          {getShortAddress()}
+        </Button>
+      ) : (
+        <Button
+          onClick={handleClickWhileInactive}
+          style={styles.walletButton}
+          size="large">
+          Connect a wallet
+        </Button>
+      )}
+    </>
+  );
+}
+
+const styles : {[key: string]: React.CSSProperties} = {
+  walletButton: {
+    backgroundColor: "#F2F2F2",
+    color: "#4F4F4F",
+    borderRadius: 12,
+    lineHeight: "24px",
+    marginLeft: 24,
+    marginRight: 24,
+  }
+}
--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,3 +1,5 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from "@web3-react/core";
 import Layout, { Content } from "antd/lib/layout/layout";
 import { PageProps } from 'gatsby';
 import * as React from "react";
@@ -5,6 +7,12 @@ import { Header } from '../components/Header';
 import { MultiMagnetForm } from '../components/mint/MultiMagnetForm';
 import { ThemeProvider } from '../components/ThemeProvider';
 import { MagnetDefinition } from '../types/magnet';
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
 
 // markup
 const MintPage : React.FC<PageProps> = (props) => {
@@ -17,18 +25,20 @@ const MintPage : React.FC<PageProps> = (props) => {
   })();
 
   return (
-    <ThemeProvider>
-      <Layout>
-        <Header />
-        <Content  style={styles.content}>
-          <div style={styles.title}>Attract and retain contributors</div>
-          <div style={styles.tip}>You can mint (create) vesting, streaming, or gift magnets. Feel free to mix and match!</div>
-          <div style={styles.mintTitle}>Mint Magnets</div>
-          <MultiMagnetForm initialSelection={initialSelection}/>
-        </Content>
-      </Layout>
-    </ThemeProvider>
-  )
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <ThemeProvider>
+        <Layout>
+          <Header />
+          <Content  style={styles.content}>
+            <div style={styles.title}>Attract and retain contributors</div>
+            <div style={styles.tip}>You can mint (create) vesting, streaming, or gift magnets. Feel free to mix and match!</div>
+            <div style={styles.mintTitle}>Mint Magnets</div>
+            <MultiMagnetForm initialSelection={initialSelection}/>
+          </Content>
+        </Layout>
+      </ThemeProvider>
+    </Web3ReactProvider>
+  );
 }
 
 export default MintPage
